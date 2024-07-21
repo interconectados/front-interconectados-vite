a4dc608510f0ceb4b2b9b44c089fcc0cadcf57c9
celestroccayoli <celestetroccayoli@gmail.com>
2024-07-21 17:33:23 -0300

    cambio en la logica de partida

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,47 +2,33 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import questions from "./questionsReflection";
 
-
 function Game() {
   const { category } = useParams();
   const navigate = useNavigate();
   const [question, setQuestion] = useState(null);
-  const [showFeedback, setShowFeedback] = useState(false);
+  const [usedQuestions, setUsedQuestions] = useState([]);
+
+  const fetchQuestion = () => {
+    const filteredQuestions = questions.filter(
+      (q) => q.category === category && !usedQuestions.includes(q.id)
+    );
+    if (filteredQuestions.length === 0) {
+      setQuestion({ text: "No hay más preguntas en esta categoría" });
+      return;
+    }
+    const randomQuestion =
+      filteredQuestions[Math.floor(Math.random() * filteredQuestions.length)];
+    setQuestion(randomQuestion);
+    setUsedQuestions((prevUsedQuestions) => [...prevUsedQuestions, randomQuestion.id]);
+  };
 
   useEffect(() => {
-    const fetchQuestion = () => {
-      const filteredQuestions = questions.filter(
-        (q) => q.category === category
-      );
-      const randomQuestion =
-        filteredQuestions[Math.floor(Math.random() * filteredQuestions.length)];
-      setQuestion(randomQuestion);
-    };
+    setUsedQuestions([]);  // Reset the used questions when category changes
     fetchQuestion();
   }, [category]);
 
-  const handleFeedback = (type) => {
-    // Actualizar los likes/dislikes de la pregunta
-    if (type === "like") {
-      question.likes += 1;
-    } else if (type === "dislike") {
-      question.dislikes += 1;
-    }
-
-    // Mostrar la siguiente pregunta después de un segundo
-    setTimeout(() => {
-      setShowFeedback(false);
-      const filteredQuestions = questions.filter(
-        (q) => q.category === category
-      );
-      const randomQuestion =
-        filteredQuestions[Math.floor(Math.random() * filteredQuestions.length)];
-      setQuestion(randomQuestion);
-    }, 1000);
-  };
-
-  const showFeedbackScreen = () => {
-    setShowFeedback(true);
+  const handleNextQuestion = () => {
+    fetchQuestion();
   };
 
   if (!question) {
@@ -58,35 +44,17 @@ function Game() {
 
   return (
     <div className="game">
-      {!showFeedback ? (
-        <div onClick={showFeedbackScreen}>
-          <p>Responde:</p>
-          <br />
-          <h2 className="question">{question.text}</h2>
-          <br />
-          <button onClick={showFeedbackScreen} style={{}}>
+      <div>
+        <p>Responde:</p>
+        <br />
+        <h2 className="question">{question.text}</h2>
+        <br />
+        {question.id && (
+          <button onClick={handleNextQuestion} style={{}}>
             Siguiente
           </button>
-        </div>
-      ) : (
-        <div>
-          <h2>¿Te gustó?</h2>
-          <button
-            className="btn no"
-            style={{}}
-            onClick={() => handleFeedback("dislike")}
-          >
-            No
-          </button>
-          <button
-            className="btn si"
-            style={{}}
-            onClick={() => handleFeedback("like")}
-          >
-            Si
-          </button>
-        </div>
-      )}
+        )}
+      </div>
       <div>
         <button className="btn out" onClick={() => navigate("/")}>
           Salir
