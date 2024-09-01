f720640bbd5a413caf8240c54e55088ca3147c39
Agustin Lacorazza <agustinpablom@gmail.com>
2024-09-01 12:32:55 -0300

    add viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces de red
+    port: 5173, // Configura el puerto 5173
+    strictPort: true // Falla si el puerto 5173 está en uso, en lugar de intentar otro puerto
+  }
 })
